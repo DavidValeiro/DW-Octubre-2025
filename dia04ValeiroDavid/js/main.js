43a8c203f738b1ec896188584d37c2add3984c67
DavidValeiro <davidvaleiro@gmail.com>
2025-10-23 18:54:55 +0200

    23 Octubre

--- dia04ValeiroDavid/js/main.js
+++ dia04ValeiroDavid/js/main.js
@@ -4,4 +4,7 @@ let roundButton = document.getElementById("roundButton");
 
 roundButton.addEventListener('click',()=>{
     imgRound.classList.toggle("youSpinMeRound");
+    roundButton.disabled = true;
+    setTimeout(function() {roundButton.disabled = false;}, 2200);
+    setTimeout(() => { imgRound.classList.remove('youSpinMeRound');}, 2200);
 });
---
+++ dia06ValeiroDavid/js/main.js
@@ -0,0 +1,7 @@
+console.log("Script enlazado");
+let almondAlert = document.getElementById("almond-death");
+
+almondAlert.addEventListener('click',()=>{
+   window.location.href = "https://tanatoriosevilla.info/"; 
+});
+
